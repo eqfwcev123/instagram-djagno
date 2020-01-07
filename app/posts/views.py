2992e9117f755722ff4e870f15324b037037560b
eqfwcev123 <dohyeonee95@hotmail.com>
2020-01-07 19:04:14 +0900

    wrote post_like def method

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 # Create your views here.
-from posts.models import Post
+from posts.models import Post, PostLike
 
 
 def post_list(request):
@@ -10,4 +9,16 @@ def post_list(request):
     context = {
         'posts': posts
     }
-    return render(request, 'posts/post-list.html',context)
+    return render(request, 'posts/post-list.html', context)
+
+
+def post_like(request, pk):
+    user = request.user
+    post = Post.objects.get(pk=pk)
+    post_like_qs = PostLike.objects.filter(post=post, user=user)
+    if post_like_qs.exists():
+        post_like_qs.delete()
+        return redirect('posts:post-list')
+    else:
+        PostLike.objects.create(post=post, user=user)
+        return redirect('posts:post-list')
