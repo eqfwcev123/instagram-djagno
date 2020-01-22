df9610bc54f15210e3d636a0c5d649981daa958c
eqfwcev123 <dohyeonee95@hotmail.com>
2020-01-22 14:47:01 +0900

    urls, views and templates changed

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
 from django.urls import path
 
-from members.views import login_view, signup_view, logout_view
+from members.views import login_view, logout_view, naver_login
 
 app_name = 'members'
 urlpatterns = [
     path('', login_view, name='login'),
     path('logout/', logout_view, name='post-logout'),
+    path('naver-login/', naver_login, name='naver-login')
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,5 +1,7 @@
+import urllib
 from datetime import datetime
 
+import requests
 from django.contrib.auth import authenticate, login, logout
 from django.forms import formset_factory
 from django.http import HttpResponse
@@ -21,8 +23,22 @@ def login_view(request):
         # GET을 할때는 form 형식만 보내주면 되기 때문에 LoginForm 내부에 request.POST를 안넣어 줘도 된다.
         form = LoginForm()
 
+    login_base_url = 'https://nid.naver.com/oauth2.0/authorize'
+    login_params = {
+        'response_type': 'code',
+        'client_id': 'DTs2e7pHYcuPAmfCI7Kg',
+        'redirect_url': 'http://localhost:8000/members/naver-login/',
+        'state': 'RANDOM_STATE',
+    }
+    login_url = '{base}?{params}'.format(
+        base=login_base_url,
+        params='&'.join([f'{key}={value}' for key, value in login_params.items()])
+    )
+    print('유알엘은 : ', login_url)
+
     context = {
-        'form': form
+        'form': form,
+        'login_url': login_url,
     }
     return render(request, 'members/login.html', context)
 
@@ -50,3 +66,52 @@ def logout_view(request):
     """
     logout(request)
     return redirect('members:login')
+
+
+def naver_login(request):
+    # GET Parameter로 전달된 code값을 사용해서
+    # 네이버 API의 php 샘플 코드를 보고
+    # token_url을 생성
+    # print(token_url)
+    login_base_url = "https://nid.naver.com/oauth2.0/token"
+    login_params = {
+        'grant_type': 'authorization_code',
+        'client_id': 'DTs2e7pHYcuPAmfCI7Kg',
+        'client_secret': '8ZtHtnt7B9',
+        'redirect_url': 'http://localhost:8000/members/naver-login/',
+        'code': request.GET['code'],
+        'state': request.GET['state'],
+    }
+
+    # ClientID는 공개값
+    # 네이버 로그인 버튼 <a href="....client_id=<Client_Id>9"
+    # ClientSecret는 비공개 값
+
+    token_url = '{base}?{params}'.format(
+        base=login_base_url,
+        params='&'.join([f'{key}={value}' for key, value in login_params.items()])
+    )
+
+    print('토큰 유알엘은: ', token_url)
+    # resposne 에 token_url 에 대한 응답이 저장되어 있다. response 에 저장되는 것은 response status이다
+    response = requests.get(token_url)
+    print(response)
+    print('상태: ', response.status_code)
+    access_token = response.json()['access_token']
+    me_header = {
+        'Authorization': f"Bearer {access_token}"
+    }
+    me_url = "https://openapi.naver.com/v1/nid/me"
+    me_response = requests.get(me_url, headers=me_header)
+    me_response_data = me_response.json()
+    print(me_response_data)
+    unique_id = me_response_data['response']['id']
+    print(unique_id)
+
+    naver_username = f'n_{unique_id}'
+    if not User.objects.filter(username=naver_username).exists():
+        user = User.objects.create_user(username=naver_username)
+    else:
+        user = User.objects.get(username=naver_username)
+    login(request, user)
+    return redirect('posts:post-list')
