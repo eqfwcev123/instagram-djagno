d712a747f2658f1f2b78d96debbece6cdff481d7
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-04 20:02:18 +0900

    python file to run application in server

---
+++ docker-run-secrets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import subprocess
+
+DOCKER_OPTIONS = [
+    ('--rm', ''),
+    ('-it', ''),
+    ('-d', ''),
+    ('-p', '8001:80'),  # 컨테이너 내부의 80번과 연결
+    ('--name', 'instagram'),
+]
+
+DOCKER_IMAGE_TAG = 'eqfwcev123/docker-wps12'
+
+# 도커 컴퓨터 생성
+subprocess.run(f'docker build -t {DOCKER_IMAGE_TAG} -f Dockerfile .', shell=True)
+# 기존에 실행중인 컨테이너 실행 중지
+subprocess.run(f'docker stop instagram', shell=True)
+
+# secrets.json이 없는 상태로 docker run으로 bash를 실행 -> background로 들어감
+# 이전에 설치한 도커 컴퓨터 실행
+subprocess.run('docker run {options} {tag} /bin/bash'.format(
+    options=' '.join([
+        f'{key} {value}' for key, value in DOCKER_OPTIONS
+    ]),
+    tag=DOCKER_IMAGE_TAG,
+), shell=True)
+
+# 실행중인 도커 컴퓨터에 secrets.json을 전송
+subprocess.run('docker cp secrets.json instagram:/srv/instagram', shell=True)
+
+# 다시 /bin/bash를 실행 시켜준다
+subprocess.run('docker exec -it instagram /bin/bash', shell=True)
+
+# 그다음에 gunicorn으로 연결을 해준다.
