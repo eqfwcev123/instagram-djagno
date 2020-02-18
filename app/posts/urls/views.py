3de9b4d144a117f8a99471b72f52b4c651319014
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-18 15:58:27 +0900

    code refactoring views

---
+++ app/posts/urls/views.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .. import views
+
+app_name = 'posts'
+urlpatterns = [
+    path('', views.post_list, name='post-list'),
+    path('<int:pk>/like/', views.post_like, name='post-like'),
+    path('create/', views.post_create, name='post-create'),
+    path('<int:post_pk>/comments/create/', views.comment_create, name='post-comment'),
+]
