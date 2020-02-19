ff913ca24b3fd75c168c586134d7f274ec9c16eb
eqfwcev123 <dohyeonee95@hotmail.com>
2020-02-19 18:57:39 +0900

    test view url routing

--- app/posts/urls/views.py
+++ app/posts/urls/views.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:pk>/like/', views.post_like, name='post-like'),
     path('create/', views.post_create, name='post-create'),
     path('<int:post_pk>/comments/create/', views.comment_create, name='post-comment'),
+    path('<int:post_pk>/comments/', views.comment_list, name='post-comment-list'),
 ]
